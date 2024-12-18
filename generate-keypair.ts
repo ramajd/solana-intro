51b86da42ad1abbb028e06a8c9e1cb13375e0f1d
Reza Alizadeh Majd <r.a.majd@gmail.com>
2024-12-18 13:27:48 +0330

    load save key on env

--- generate-keypair.ts
+++ generate-keypair.ts
@@ -1,7 +1,25 @@
-import { Keypair } from '@solana/web3.js'
+import * as fs from 'fs'
+import 'dotenv/config'
+import { Keypair, PublicKey } from '@solana/web3.js'
+import { getKeypairFromEnvironment } from '@solana-developers/helpers'
 
-const keypair = Keypair.generate()
+function generateNewKey() {
+	const key = Keypair.generate();
+	console.log(`🔆 New key generated: ${key.publicKey.toBase58()}`)
+	fs.writeFileSync('.env', `SECRET_KEY=[${key.secretKey}]`)
+	console.log(`🔆 key saved in ENV`)
+	return keypair
+}
 
-console.log(`The public key is: ${keypair.publicKey.toBase58()}`)
-console.log(`The secret key is: ${keypair.secretKey}`)
-console.log(`✅ Finished!`)
+function loadKey() {
+	if (!process.env.SECRET_KEY)
+		return null
+	const key = getKeypairFromEnvironment('SECRET_KEY')
+	console.log(`🔆 Existing key loaded from ENV: ${key.publicKey.toBase58()}`)
+	return key
+}
+
+
+// === MAIN ==========
+const keypair = loadKey() || generateNewKey()
+console.log(`✅ Account loaded.`)
